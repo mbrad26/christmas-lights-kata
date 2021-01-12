b9cb7f2fa3fd8ea4dd9d0d4220d17c8b0e520c50
mbrad26 <mbrad26@gmail.com>
2021-01-12 13:26:13 +0000

    turns all lights ON in a row

--- spec/lightsGrid.test.js
+++ spec/lightsGrid.test.js
@@ -12,7 +12,7 @@ describe('LightsGrid', () => {
 
   it('instantiates with a 10 x 10 grid of lights', () => {
     const lightsGrid = new LightsGrid(Light);
-    const grid = new Array(5).fill(new Array(5).fill(new Light()));
+    const grid = new Array(10).fill(new Array(10).fill(new Light()));
 
     expect(lightsGrid.grid.toString()).toEqual(grid.toString());
   });
@@ -23,8 +23,8 @@ describe('LightsGrid', () => {
 
       lightsGrid.turnONLights(0, 0, 0, 0);
 
-      for(let i = 0; i < 5; i++) {
-        for(let j = 0; j < 5; j++) {
+      for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 10; j++) {
           if(i === 0 && j === 0) {
             expect(lightsGrid.grid[i][j].state).toEqual('ON');
           } else {
@@ -39,8 +39,25 @@ describe('LightsGrid', () => {
 
       lightsGrid.turnONLights(0, 0, 0, 1);
 
-      for(let i = 0; i < 5; i++) {
-        for(let j = 0; j < 5; j++) {
+      for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 10; j++) {
+          if(i === 0 && j === 0) {
+            expect(lightsGrid.grid[i][j].state).toEqual('ON');
+            expect(lightsGrid.grid[i][j+1].state).toEqual('ON');
+            break;
+          };
+          expect(lightsGrid.grid[i][j].state).toEqual('OFF');
+        };
+      };
+    });
+
+    it('turns all lights ON in a row', () => {
+      const lightsGrid = new LightsGrid(Light);
+
+      lightsGrid.turnONLights(0, 0, 0, 10);
+
+      for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 10; j++) {
           if(i === 0 && j === 0) {
             expect(lightsGrid.grid[i][j].state).toEqual('ON');
             expect(lightsGrid.grid[i][j+1].state).toEqual('ON');
